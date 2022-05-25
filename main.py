a5030fa2b4101d530d77f460705ed179cc31adfa
Jeffboss2233 <105881994+Jeffboss2233@users.noreply.github.com>
2022-05-25 20:32:40 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+print("hello, welcome to olivers backry")
+
+name = input("what is your name?")
+
+if name == "ben":
+    evil_status = input("are you evil")
+    if evil_status == "yes"
+        print("your not welcome here, get out")
+        exit()
+    else:
+        print("oh so your one of those good bens come in")
+else:
+   print("hello" + name +", thank you so much for coming in today.")
+   
+   
+   
+menu = "black coffees, lattes, coffees, frappuccino"
+   
+print(name +", what would you like from our menu today" + menu)
+   
+order = input()
+
+much = input("how much coffees would you like?")
+
+if order = "frappuccino":
+    price = 13
+elif order == "black coffees":
+    price = 3
+elif order == "lattes":
+    price = 12
+elif order == "coffees":
+    price = 10
+else:
+    print("sorry we dont have that")
+    
+total = price * int(much)
+print(total)
+
+print("thank you your total is: £" =str(total))
+
+print(" that sounds good" + name + "we will have your" + much +" "+ order +", ready in a min")
+   
+   
